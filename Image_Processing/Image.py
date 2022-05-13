106668ce15d3a7ba8e040c0f939329f162cd0835
MichaelSamelsohn <michael.samelsohn@gmail.com>
2022-05-13 13:41:28 +0300

    Added basic functionality for Image class.

--- Image_Processing/Image.py
+++ Image_Processing/Image.py
@@ -7,7 +7,10 @@ Created by Michael Samelsohn, 12/05/22
 """
 
 # Imports #
+import copy
+
 import matplotlib.image as im
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -22,27 +25,85 @@ class Image:
     def __init__(self, image_path=Settings.DEFAULT_IMAGE_LENA):
         # TODO: Add support for initialization using image array.
 
-        self.__image_path = image_path
-        self.__check_image_existence()
+        self.__original_image = None
 
-        self.__original_image = im.imread(fname=self.__image_path)
+        self.__image_path = image_path
+        self.__load_original_image()
+
         self.__image = im.imread(fname=self.__image_path)
 
-    def __check_image_existence(self):
+    def __load_original_image(self):
         """
-        Change the path to the specified directory (relevant for later when the images are being saved).
-        If specified path does not exist, default to 'Images' directory.
+        Check if image path exists. If not, use default image (Lena).
+        Once path is established, load the image.
         """
 
         log.debug(f"The selected directory is - {self.__image_path}")
         if os.path.exists(path=self.__image_path):
+            self.__original_image = im.imread(fname=self.__image_path)
             log.info(f"Image, {self.__image_path}, exists")
             return True
         else:
             log.error(f"Image, {self.__image_path}, doesn't exist, will use Lena image")
             self.__image_path = Settings.DEFAULT_IMAGE_LENA
+            self.__original_image = im.imread(fname=self.__image_path)
             return False
+
+    @property
+    def image_path(self):
+        """
+        Get the image path.
+        :return: The image path.
+        """
+        return self.__image_path
+
+    @image_path.setter
+    def image_path(self, new_image_path):
+        """
+        Set the image path.
+        :param new_image_path: The new image path.
+        """
+        self.__image_path = new_image_path
+        self.__load_original_image()
+
+    def reset_to_original_image(self):
+        """
+        Reset the edited image to the original one.
+        """
+
+        log.warning("Resetting the edited image to original one")
+        self.__image = copy.deepcopy(self.__original_image)
+
+    def display_original_image(self):
+        """
+        Display the original image.
+        """
+
+        log.debug("Displaying the original image")
+        plt.imshow(self.__original_image)
+        plt.show()
+
+    def display_image(self):
+        """
+        Display the edited image.
+        """
+
+        log.debug("Displaying the edited image")
+        plt.imshow(self.__image)
+        plt.show()
+
+    def compare_to_original(self):
+        """
+        Display the edited image in comparison with the original one.
+        """
+
+        log.debug("Displaying the original and edited images side-by-side for comparison")
+        figure, axs = plt.subplots(1, 2)
+        axs[0].imshow(self.__original_image)
+        axs[1].imshow(self.__image)
+        plt.show()
 
 
 if __name__ == "__main__":
     obj = Image("/Users/michaelsamelsohn/PycharmProjects/Data_Processing/Images/Lena.png")
+    obj.compare_to_original()
