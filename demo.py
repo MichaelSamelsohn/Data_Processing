c9a7d31cd3f7de24776737201f9a605b806d4671
MichaelSamelsohn <michael.samelsohn@gmail.com>
2024-04-12 23:58:11 +0300

    Tweaks to the demo.

--- demo.py
+++ demo.py
@@ -29,18 +29,20 @@ def nasa_api_demo():
         4) NIL (NASA Imaging Library) with an option to select the query (NIL_QUERY).
     """
 
+    log.info("Demo showcasing the NASA API")
+
     # APOD (Astronomy Picture Of the Day) demo.
     apod_date = input("Enter APOD date (acceptable format is - YYYY-MM-DD): ")
     apod = APOD(image_directory=IMAGE_DIRECTORY_PATH, date=apod_date)
     apod.astronomy_picture_of_the_day()
-    apod.log_class_parameters()
+    apod._log_class_parameters()
     image = Image(image_path=os.path.join(IMAGE_DIRECTORY_PATH, f"APOD_{apod_date}.JPG"))
     image.display_original_image()
 
     # EPIC (Earth Polychromatic Imaging Camera) demo.
     epic = EPIC(image_directory=IMAGE_DIRECTORY_PATH, number_of_images=1)
     epic.earth_polychromatic_imaging_camera()
-    epic.log_class_parameters()
+    epic._log_class_parameters()
     image = Image(image_path=os.path.join(IMAGE_DIRECTORY_PATH, "EPIC.png"))
     image.display_original_image()
 
@@ -49,7 +51,7 @@ def nasa_api_demo():
     mars_date = input("Enter rover date (acceptable format is - YYYY-MM-DD): ")
     mars = MARS(image_directory=IMAGE_DIRECTORY_PATH, rover=mars_rover, date=mars_date,  number_of_images=1)
     mars.mars_rover_images()
-    mars.log_class_parameters()
+    mars._log_class_parameters()
     image = Image(image_path=os.path.join(IMAGE_DIRECTORY_PATH, "MARS.JPG"))
     image.display_image()
 
@@ -57,21 +59,19 @@ def nasa_api_demo():
     nil_query = input("Enter query: ")
     nil = NIL(image_directory=IMAGE_DIRECTORY_PATH, query=nil_query)
     nil.nasa_image_library_query()
-    nil.log_class_parameters()
+    nil._log_class_parameters()
     image = Image(image_path=os.path.join(IMAGE_DIRECTORY_PATH, f"NIL_{nil_query.replace(' ', '_')}.JPG"))
     image.display_image()
 
 
 def intensity_transformations_demo():
+    log.debug("Demo showcasing the image processing capabilities")
+
     # Conversion to greyscale and histogram display.
     lena = Image(image_path=LENA_IMAGE_PATH)
     lena.transform_image(transformation_type="convert_to_grayscale", image=lena.image)
 
     # Showcase different transforms.
-    log.info("Please specify the following rubric - <transformation_type> <additional parameters>")
-    log.info("If no additional parameters are specified, default ones will be used")
-    log.info("")
-
     log.debug("Available transformation types are:")
     log.debug("Intensity transformations category - Changes made on a single pixel basis, as opposed to pixel "
               "neighbourhood based operations")
@@ -90,32 +90,24 @@ def intensity_transformations_demo():
     log.debug("")
 
     log.debug("Segmentation - ??")
-    log.debug("\tglobal_thresholding - ??")
-    log.debug("\tlaplacian_gradient - ??")
-    log.debug("\tisolated_point_detection - ??")
-    log.debug("\tline_detection - ??")
-    log.debug("\tkirsch_edge_detection - ??")
+    log.debug("\tglobal_thresholding - Same as regular thresholding, only the threshold is calculated depending on the "
+              "image and the seed")
+    log.debug("\tlaplacian_gradient - Second derivative of the image")
+    log.debug("\tisolated_point_detection - Detect and highlight isolated points in the image")
+    log.debug("\tline_detection - Detect and highlight lines in the image")
+    log.debug("\tkirsch_edge_detection - Edge detection and highlighting for all 8 compass directions")
     log.debug("")
 
-    # TODO: To be implemented.
-    # log.debug("Morphological - Convolution with a custom structuring element")
-    # log.debug("\tmorphological_erosion - ??")
-    # log.debug("\tmorphological_dilation - ??")
-    # log.debug("\tmorphological_opening - ??")
-    # log.debug("\tmorphological_closing - ??")
-    # log.debug("\tboundary_extraction - ??")
-    # log.debug("\tmorphological_convolution - ??")
-    # log.debug("\tmorphological_opening - ??")
-    # log.debug("")
-
-    transformation_type = input("Transformation type: ")
+    transformation_type = input("Transformation type - ")
     lena.transform_image(transformation_type=transformation_type, image=lena.image)
     lena.display_all_images()
 
 
 if __name__ == '__main__':
     # NASA API demo.
-    # nasa_api_demo()
+    nasa_api_demo()
 
     # Image processing demo.
     intensity_transformations_demo()
+
+
