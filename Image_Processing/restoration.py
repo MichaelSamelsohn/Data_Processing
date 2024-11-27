28c513e7ac4663783e5a97782ab586433883f223
Michael <michael.samelsohn@gmail.com>
2024-11-27 17:17:19 +0200

    Refactored how mean filtering is done.

--- Image_Processing/restoration.py
+++ Image_Processing/restoration.py
@@ -56,34 +56,67 @@ def salt_and_pepper(image: ndarray, pepper=0.001, salt=0.001) -> ndarray:
 
 
 @book_reference(book=image_settings.GONZALES_WOODS_BOOK,
-                reference="Chapter 5.3 - Restoration in the Presence of Noise Only—Spatial Filtering, p.328")
-def geometric_mean_filter(image: ndarray, padding_type=image_settings.DEFAULT_PADDING_TYPE,
-                          filter_size=image_settings.DEFAULT_FILTER_SIZE) -> ndarray:
+                reference="Chapter 5.3 - Restoration in the Presence of Noise Only—Spatial Filtering, p.328-330")
+def mean_filter(image: ndarray, filter_type=image_settings.DEFAULT_MEAN_FILTER_TYPE,
+                padding_type=image_settings.DEFAULT_PADDING_TYPE, filter_size=image_settings.DEFAULT_FILTER_SIZE) \
+        -> ndarray:
     """
-    A geometric mean filter achieves smoothing comparable to an arithmetic mean (box kernel) filter, but it tends to
-    lose less image detail in the process.
+    TODO: Complete the docstring.
+
+    Note - Since more computations are done than necessary, this implementation is not optimal, rather for simplicity
+    of use.
 
     :param image: The image for filtering.
+    :param filter_type: The filter type used for the mean filtering.
     :param padding_type: Padding type used for applying the filter.
     :param filter_size: The filter size used for the image restoration.
 
     :return: Filtered image.
     """
 
-    log.info("Applying a geometric mean filter on the image")
+    log.info(f"Applying a {filter_type} mean filter on the image")
 
     # Padding the image so the kernel can be applied to the image boundaries.
     padded_image = pad_image(image=image, padding_type=padding_type, padding_size=filter_size // 2)
 
     log.debug("Scanning the padded image and assigning the geometric mean pixel value for each scanned pixel")
-    geometric_mean_image = np.zeros(shape=image.shape)
+    mean_filter_image = np.zeros(shape=image.shape)
     for row in range(filter_size // 2, image.shape[0] + filter_size // 2):
         for col in range(filter_size // 2, image.shape[1] + filter_size // 2):
             # Extract the sub-image.
             sub_image = extract_sub_image(image=padded_image, position=(row, col), sub_image_size=filter_size)
-            # Finding the geometric mean value of the sub-image and assign it.
-            power = 1 / np.power(filter_size, 2)
-            geometric_mean_image[row - filter_size // 2][col - filter_size // 2] = np.power(np.prod(sub_image), power)
+            # Finding the appropriate mean value of the sub-image and assigning it.
+            match filter_type:
+                case "arithmetic":
+                    """
+                    The arithmetic mean filter is the simplest of the mean filters (the arithmetic mean filter is the 
+                    same as the box filter).
+                    """
+                    mean_filter_image[row - filter_size // 2][col - filter_size // 2] = np.average(sub_image)
+                case "geometric":
+                    """
+                    A geometric mean filter achieves smoothing comparable to an arithmetic mean (box kernel) filter, but 
+                    it tends to lose less image detail in the process.
+                    """
+                    power = 1 / np.power(filter_size, 2)
+                    mean_filter_image[row - filter_size // 2][col - filter_size // 2] = \
+                        np.power(np.prod(sub_image), power)
+                case "harmonic":
+                    """
+                    The harmonic mean filter works well for salt noise, but fails for pepper noise. It does well also 
+                    with other types of noise like Gaussian noise.
+                    """
+                    pass  # TODO: To be implemented.
+                case "contraharmonic":
+                    """
+                    This filter is well suited for reducing or virtually eliminating the effects of salt-and-pepper 
+                    noise. For positive values of Q, the filter eliminates pepper noise. For negative values of Q, it 
+                    eliminates salt noise. It cannot do both simultaneously. Note that the contraharmonic filter reduces 
+                    to the arithmetic mean filter if Q = 0, and to the harmonic mean filter if Q = −1.
+                    """
+                    pass  # TODO: To be implemented.
+
+    return mean_filter_image
 
     return geometric_mean_image
 
