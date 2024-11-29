14265679388c968d3849319b2a52b44fb3ac4b2a
Michael <michael.samelsohn@gmail.com>
2024-11-29 16:08:16 +0200

    Refactored how order statistic filter work.

--- Image_Processing/restoration.py
+++ Image_Processing/restoration.py
@@ -86,6 +86,7 @@ def mean_filter(image: ndarray, filter_type=image_settings.DEFAULT_MEAN_FILTER_T
         for col in range(filter_size // 2, image.shape[1] + filter_size // 2):
             # Extract the sub-image.
             sub_image = extract_sub_image(image=padded_image, position=(row, col), sub_image_size=filter_size)
+
             # Finding the appropriate mean value of the sub-image and assigning it.
             match filter_type:
                 case "arithmetic":
@@ -144,23 +145,17 @@ def mean_filter(image: ndarray, filter_type=image_settings.DEFAULT_MEAN_FILTER_T
 
     return mean_filter_image
 
-    return geometric_mean_image
-
 
 @book_reference(book=image_settings.GONZALES_WOODS_BOOK,
                 reference="Chapter 5.3 - Restoration in the Presence of Noise Only—Spatial Filtering, p.330-332")
-def median_filter(image: ndarray, padding_type=image_settings.DEFAULT_PADDING_TYPE,
-                  filter_size=image_settings.DEFAULT_FILTER_SIZE) -> ndarray:
+def order_statistic_filter(image: ndarray, filter_type=image_settings.DEFAULT_ORDER_STATISTIC_FILTER_TYPE,
+                           padding_type=image_settings.DEFAULT_PADDING_TYPE,
+                           filter_size=image_settings.DEFAULT_FILTER_SIZE) -> ndarray:
     """
-    The best-known order-statistic filter in image processing is the median filter, which, as its name implies, replaces
-    the value of a pixel by the median of the intensity levels in a predefined neighborhood of that pixel. The value of
-    the pixel is included in the computation of the median.
-
-    Median filters are quite popular because, for certain types of random noise, they provide excellent noise-reduction
-    capabilities, with considerably less blurring than linear smoothing filters of similar size. Median filters are
-    particularly effective in the presence of both bipolar and unipolar impulse noise.
+    TODO: Expand on order-statistic filters.
 
     :param image: The image for filtering.
+    :param filter_type: The filter type used for the mean filtering.
     :param padding_type: Padding type used for applying the filter.
     :param filter_size: The filter size used for the image restoration.
 
@@ -178,8 +173,28 @@ def median_filter(image: ndarray, padding_type=image_settings.DEFAULT_PADDING_TY
         for col in range(filter_size // 2, image.shape[1] + filter_size // 2):
             # Extract the sub-image.
             sub_image = extract_sub_image(image=padded_image, position=(row, col), sub_image_size=filter_size)
-            # Finding the median value of the sub-image and assign it.
-            flat_sub_image = np.ndarray.flatten(sub_image)
-            median_image[row - filter_size // 2][col - filter_size // 2] = np.sort(flat_sub_image)[filter_size**2 // 2]
+            # Flattening and sorting the sub-image.
+            sorted_flat_sub_image = np.sort(np.ndarray.flatten(sub_image))
+
+            # Finding the order statistic value of the sub-image and assigning it.
+            match filter_type:
+                case "median":
+                    """
+                    The best-known order-statistic filter in image processing is the median filter, which, as its name 
+                    implies, replaces the value of a pixel by the median of the intensity levels in a predefined 
+                    neighborhood of that pixel. The value of the pixel is included in the computation of the median.
+                    Median filters are quite popular because, for certain types of random noise, they provide excellent 
+                    noise-reduction capabilities, with considerably less blurring than linear smoothing filters of 
+                    similar size. Median filters are particularly effective in the presence of both bipolar and unipolar 
+                    impulse noise.
+                    """
+                    median_image[row - filter_size // 2][col - filter_size // 2] = \
+                        sorted_flat_sub_image[filter_size**2 // 2]
+                case "max":
+                    pass  # TODO: To be implemented.
+                case "min":
+                    pass  # TODO: To be implemented.
+                case "midpoint":
+                    pass  # TODO: To be implemented.
 
     return median_image
