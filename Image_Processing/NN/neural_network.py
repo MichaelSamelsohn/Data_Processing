aa3aae19e2ade0f7b6d1bca2d8eaf212ccb38de3
Michael <michael.samelsohn@gmail.com>
2025-01-31 16:14:46 +0200

    Added logger functionality to neural_network.py.

--- Image_Processing/NN/neural_network.py
+++ Image_Processing/NN/neural_network.py
@@ -8,6 +8,7 @@ Created by Michael Samelsohn, 31/01/25
 
 # Imports #
 import numpy as np
+from Settings.settings import log
 
 
 def activation_function(x):
@@ -23,15 +24,16 @@ def activation_function_derivative(x):
 # Define the neural network class
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
+        log.debug("Initializing a neural network class")
         self.hidden_layer_input = None
         self.hidden_layer_output = None
         self.output_layer_input = None
 
-        # Initialize weights randomly with mean 0.
+        log.debug("Initialize weights randomly with mean 0")
         self.weights_input_hidden = np.random.uniform(-1, 1, (input_size, hidden_size))
         self.weights_hidden_output = np.random.uniform(-1, 1, (hidden_size, output_size))
 
-        # Initialize biases.
+        log.debug("Initialize biases")
         self.bias_hidden = np.zeros((1, hidden_size))
         self.bias_output = np.zeros((1, output_size))
 
@@ -40,11 +42,12 @@ class NeuralNetwork:
         In the feedforward method, the data flows through the network. It calculates the weighted sums for the hidden
         and output layers and applies the activation function.
         """
-        # Calculate activations for hidden layer
+
+        # Calculate activations for hidden layer.
         self.hidden_layer_input = np.dot(x, self.weights_input_hidden) + self.bias_hidden
         self.hidden_layer_output = activation_function(self.hidden_layer_input)
 
-        # Calculate activations for output layer
+        # Calculate activations for output layer.
         self.output_layer_input = np.dot(self.hidden_layer_output, self.weights_hidden_output) + self.bias_output
         return activation_function(self.output_layer_input)
 
@@ -53,6 +56,7 @@ class NeuralNetwork:
         Compute the gradients of the weights and biases by using the chain rule and update them accordingly. First
         calculating the error at the output layer and propagating that error backward to the hidden layer.
         """
+
         # Feed-forward to get predictions.
         output = self.feed_forward(x)
 
@@ -72,15 +76,16 @@ class NeuralNetwork:
 
     def train(self, x, y, epochs, learning_rate):
         """
-         Runs for a specified number of epochs, calling the back-propagation method on each epoch, and updating the
-         weights and biases. Every 1000 epochs, the loss (mean squared error) is printed to show how the model is
-         improving.
+        Runs for a specified number of epochs, calling the back-propagation method on each epoch, and updating the
+        weights and biases. Every 1000 epochs, the loss (mean squared error) is printed to show how the model is
+        improving.
         """
+
         for epoch in range(epochs):
             self.back_propagate(x, y, learning_rate)
             if epoch % 1000 == 0:
                 loss = np.mean(np.square(y - self.feed_forward(x)))  # Mean squared error loss
-                print(f'Epoch {epoch}, Loss: {loss}')
+                log.info(f"Epoch {epoch}, Loss: {loss}")
 
 
 # Example: AND problem (input, output)
