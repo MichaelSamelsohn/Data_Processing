0b74cd5c729d17f35050aee108dfe3478529f718
Michael <michael.samelsohn@gmail.com>
2025-07-28 08:41:20 +0300

    WiFi - Added string conversion to bits (with unit test).

--- Signals/wifi.py
+++ Signals/wifi.py
@@ -11,6 +11,37 @@ from Settings.settings import log
 
 
 def lfsr(sequence_length: int, seed=127) -> list[int]:
+def convert_string_to_bits(text: str, style='binary') -> list[int | str]:
+    """
+    Convert text string to bits according to ASCII convention - https://www.ascii-code.com/.
+
+    :param text: Text string.
+    :param style: Type of output. There are two options:
+    1) 'binary' - List of binary values where each ASCII byte is split into 8 bits from MSB to LSB with zeros prepended
+    if necessary.
+    2) 'hex' - List of bytes in string format (for example, '0xAB').
+
+    :return: List of byte values represented either as binary values or string hex values.
+    """
+
+    # Encode text to bytes using ASCII.
+    byte_data = text.encode('utf-8')
+
+    data_list = []
+    match style:
+        case 'binary':
+            # Bit list as flat list[int], each byte split into bits (MSB first).
+            for b in byte_data:
+                bits = [(b >> i) & 1 for i in reversed(range(8))]  # Extract bits from MSB to LSB.
+                data_list.extend(bits)
+        case 'hex':
+            data_list = [f"0x{b:02X}" for b in byte_data]  # Uppercase hex bytes.
+        case 'bytes':
+            data_list = list(byte_data)
+
+    return data_list
+
+
     """
     LFSR (Linear Feedback Shift Register) is a shift register whose input bit is a linear function of its previous
     state. The initial value of the LFSR is called the seed, and because the operation of the register is deterministic,
--- Tests/wifi_tests.py
+++ Tests/wifi_tests.py
@@ -3,13 +3,76 @@ import random
 import pytest
 
 from unittest.mock import patch
-from wifi import lfsr, scramble
+from wifi import generate_lfsr_sequence, scramble, convert_string_to_bits, cyclic_redundancy_check_32
 
 # Constants #
 BASIC_LFSR_SEQUENCE = [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
                        0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0,
                        1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
                        1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+# IEEE Std 802.11-2020 OFDM PHY specification, I.1.2 The message for the BCC example, p. 4150.
+MESSAGE = """Joy, bright spark of divinity,
+Daughter of Elysium,
+Fire-insired we trea"""
+# IEEE Std 802.11-2020 OFDM PHY specification, I.1.2 The message for the BCC example, p. 4150, Table I-1—The message for
+# the BCC example, octets 25-96 (included).
+# Note - These 72 bytes correspond to the 72 bytes of the message (including line breaks).
+MESSAGE_IN_BYTES = [
+    # Joy, bright spark of divinity,\n
+    0x4A, 0x6F, 0x79, 0x2C, 0x20, 0x62, 0x72, 0x69, 0x67, 0x68, 0x74, 0x20, 0x73, 0x70, 0x61, 0x72, 0x6B, 0x20, 0x6F,
+    0x66, 0x20, 0x64, 0x69, 0x76, 0x69, 0x6E, 0x69, 0x74, 0x79, 0x2C, 0x0A,
+
+    # Daughter of Elysium,\n
+    0x44, 0x61, 0x75, 0x67, 0x68, 0x74, 0x65, 0x72, 0x20, 0x6F, 0x66, 0x20, 0x45, 0x6C, 0x79, 0x73, 0x69, 0x75, 0x6D,
+    0x2C, 0x0A,
+
+    # Fire-insired we trea
+    0x46, 0x69, 0x72, 0x65, 0x2D, 0x69, 0x6E, 0x73, 0x69, 0x72, 0x65, 0x64, 0x20, 0x77, 0x65, 0x20, 0x74, 0x72, 0x65,
+    0x61
+]
+# IEEE Std 802.11-2020 OFDM PHY specification, I.1.5 Generating the DATA bits for the BCC example, p. 4160-4161,
+# Table I-13—The DATA bits before scrambling, bits 208-783 (included).
+# Note - These 576 bits correspond to the 72 bytes (each byte is 8 bits) of the message (including line breaks).
+MESSAGE_IN_BITS = [
+    0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0,
+    0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+    0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0,
+    0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1,
+    0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0,
+    1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0,
+    1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+    1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1,
+    0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1,
+    0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0,
+    1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1,
+    0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1
+]
+def test_convert_string_to_bits():
+    """
+    Test purpose - Basic functionality of converting strings to bits.
+    Criteria:
+    1) Correct data conversion when style='binary'.
+    3) Correct data conversion when style='bytes'.
+    2) Correct data conversion when style='hex'.
+
+    Test steps:
+    1) Convert test message using the 'binary' style.
+    2) Assert that generated list is bit-exact to expected outcome.
+    3) Convert test message using the 'bytes' style.
+    4) Assert that generated list is bit-exact to expected outcome.
+    5) Convert test message using the 'hex' style.
+    6) Assert that generated list is bit-exact to expected outcome.
+    """
+
+    # Steps (1)+(2) - Convert message to bits and compare to expected outcome.
+    assert convert_string_to_bits(text=MESSAGE, style='binary') == MESSAGE_IN_BITS
+    # Steps (3)+(4) - Convert message to bytes and compare to expected outcome.
+    assert convert_string_to_bits(text=MESSAGE, style='bytes') == MESSAGE_IN_BYTES
+    # Steps (5)+(6) - Convert message to hex strings and compare to expected outcome.
+    assert convert_string_to_bits(text=MESSAGE, style='hex') == [f'0x{byte:02X}' for byte in MESSAGE_IN_BYTES]
 
 
 @pytest.mark.parametrize(
