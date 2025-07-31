13f067605e293cd4579df9efc3a850529c560345
Michael.Samelsohn <michael.samelsohn@celeno.com>
2025-07-31 19:24:24 +0300

    WiFi - Moved MCS parameters to PHY script.

--- Signals/wifi.py
+++ Signals/wifi.py
@@ -6,26 +6,6 @@ TODO: Add explanation as to what this script does.
 Created by Michael Samelsohn, 19/07/25.
 """
 
-# Constants #
-MODULATION_CODING_SCHEME_PARAMETERS = {
-    6:  {"MODULATION": 'BPSK',   "CODING_RATE": '1/2', "N_BPSC": 1,
-         "N_CBPS": 48,  "N_DBPS": 24,  "SIGNAL_FIELD_CODING": [1, 1, 0, 1]},
-    9:  {"MODULATION": 'BPSK',   "CODING_RATE": '3/4', "N_BPSC": 1,
-         "N_CBPS": 48,  "N_DBPS": 36,  "SIGNAL_FIELD_CODING": [1, 1, 1, 1]},
-    12: {"MODULATION": 'QPSK',   "CODING_RATE": '1/2', "N_BPSC": 2,
-         "N_CBPS": 96,  "N_DBPS": 48,  "SIGNAL_FIELD_CODING": [0, 1, 0, 1]},
-    18: {"MODULATION": 'QPSK',   "CODING_RATE": '3/4', "N_BPSC": 2,
-         "N_CBPS": 96,  "N_DBPS": 72,  "SIGNAL_FIELD_CODING": [0, 1, 1, 1]},
-    24: {"MODULATION": '16-QAM', "CODING_RATE": '1/2', "N_BPSC": 4,
-         "N_CBPS": 192, "N_DBPS": 96,  "SIGNAL_FIELD_CODING": [1, 0, 0, 1]},
-    36: {"MODULATION": '16-QAM', "CODING_RATE": '3/4', "N_BPSC": 4,
-         "N_CBPS": 192, "N_DBPS": 144, "SIGNAL_FIELD_CODING": [1, 0, 1, 1]},
-    48: {"MODULATION": '64-QAM', "CODING_RATE": '2/3', "N_BPSC": 6,
-         "N_CBPS": 288, "N_DBPS": 192, "SIGNAL_FIELD_CODING": [0, 0, 0, 1]},
-    54: {"MODULATION": '64-QAM', "CODING_RATE": '3/4', "N_BPSC": 6,
-         "N_CBPS": 288, "N_DBPS": 216, "SIGNAL_FIELD_CODING": [0, 0, 1, 1]}
-}
-
 
 class CHIP:
     def __init__(self):
