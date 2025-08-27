f7104f05509253576f7a204a61d26eec88de48ad
Michael.Samelsohn <michael.samelsohn@celeno.com>
2025-08-27 16:01:13 +0300

    WiFi - Refactored settings file.

--- WiFi/Settings/wifi_settings.py
+++ WiFi/Settings/wifi_settings.py
@@ -1,9 +1,12 @@
 # Imports #
-from datetime import datetime
 from Utilities.logger import Logger
-log = Logger()
 
-TIME = datetime.now().strftime("%Y-%m-%d_%H%M")
+# Logger settings.
+log = Logger()
+log.add_custom_log_level("success", 25, "\x1b[32;1m")       # Bright Green.
+log.add_custom_log_level("traffic", 11, "\x1b[38;5;208m")   # Deep Violet / Grape.
+log.add_custom_log_level("channel", 12, "\x1b[38;5;5m")     # Magenta.
+log.log_level = 20
 
 HOST = '127.0.0.1'
 
