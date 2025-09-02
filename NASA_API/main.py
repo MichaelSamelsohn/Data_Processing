93f0b00d2081b7103235f8326b13aa28014b45fd
Michael.Samelsohn <michael.samelsohn@celeno.com>
2025-09-02 16:35:34 +0300

    NASA API - Refactored EPIC API.

--- NASA_API/main.py
+++ NASA_API/main.py
@@ -1,8 +1,17 @@
 # Imports #
-from NASA_API.Source.apod import APOD
 
-# APOD - Astronomy Picture Of the Day.
+from NASA_API.Settings.api_settings import log
+from NASA_API.Source.api_utilities import display_image
+from NASA_API.Source.apod import APOD
+from NASA_API.Source.epic import EPIC
+
+log.info("APOD - Astronomy Picture Of the Day")
 apod = APOD()
 apod.date = "1999-04-27"
 apod.astronomy_picture_of_the_day()
-apod.display_image()
+display_image(image_path=apod.apod_image)
+
+log.info("EPIC - Earth Polychromatic Imaging Camera")
+epic = EPIC()
+epic.earth_polychromatic_imaging_camera()
+display_image(image_path=epic.epic_image)
