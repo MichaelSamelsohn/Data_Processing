ea4aa0eeb60ca530fcce321c6a5e5a48bd0dd019
Michael <michael.samelsohn@gmail.com>
2025-10-19 18:29:23 +0300

    Monopoly - Handled trade offers that include mortgaged property.

--- Monopoly/Source/game.py
+++ Monopoly/Source/game.py
@@ -355,16 +355,31 @@ class Game:
             log.warning("Invalid player name")
             return
 
-        initiator_property_offer, initiator_cash_offer, initiator_free_cards_offer = (
+        initiator_space_offer, initiator_cash_offer, initiator_free_cards_offer = (
             self.make_offer(trade_offer_initiator))
-        recipient_property_offer, recipient_cash_offer, recipient_free_cards_offer = (
+        recipient_space_offer, recipient_cash_offer, recipient_free_cards_offer = (
             self.make_offer(trade_offer_recipient))
+
+        # Check that initiator of the trade doesn't become bankrupt after it.
+        initiator_cash_after_trade = trade_offer_initiator.cash - initiator_cash_offer + recipient_cash_offer
+        initiator_mortgage_fee = sum([0.1 * space.mortgage_value for space in recipient_space_offer
+                                      if space.is_mortgaged])
+        if initiator_cash_after_trade - initiator_mortgage_fee < 0:
+            log.warning(f"Bank declined trade offer as it will put the {trade_offer_initiator.name} in bankruptcy")
+            return
+        # Check that recipient of the trade doesn't become bankrupt after it.
+        recipient_cash_after_trade = trade_offer_recipient.cash - recipient_cash_offer + initiator_cash_offer
+        recipient_mortgage_fee = sum([0.1 * space.mortgage_value for space in initiator_space_offer
+                                      if space.is_mortgaged])
+        if recipient_cash_after_trade - recipient_mortgage_fee < 0:
+            log.warning(f"Bank declined trade offer as it will put the {trade_offer_recipient.name} in bankruptcy")
+            return
 
         log.info("--- TRADE SUMMARY ---")
         log.info(f"{trade_offer_initiator.name} offers to {trade_offer_recipient.name}: "
-                 f"{[p.name for p in initiator_property_offer]} + {initiator_cash_offer}$")
+                 f"{[p.name for p in initiator_space_offer]} + {initiator_cash_offer}$")
         log.info(f"{trade_offer_initiator.name} wants from {trade_offer_recipient.name}: "
-                 f"{[p.name for p in recipient_property_offer]} + {recipient_cash_offer}$")
+                 f"{[p.name for p in recipient_space_offer]} + {recipient_cash_offer}$")
 
         # Recipient to confirm trade offer.
         if isinstance(trade_offer_recipient, Human):
@@ -378,11 +393,11 @@ class Game:
         # Perform trade.
         self.execute_trade(
             # Player 1.
-            p1=trade_offer_initiator, p1_properties=initiator_property_offer,
-            p1_cash=initiator_cash_offer, p1_free_cards=initiator_free_cards_offer,
+            player1=trade_offer_initiator, player1_spaces=initiator_space_offer,
+            player1_cash=initiator_cash_offer, player1_free_cards=initiator_free_cards_offer,
             # Player 2.
-            p2=trade_offer_recipient, p2_properties=recipient_property_offer,
-            p2_cash=recipient_cash_offer, p2_free_cards=recipient_free_cards_offer)
+            player2=trade_offer_recipient, player2_spaces=recipient_space_offer,
+            player2_cash=recipient_cash_offer, player2_free_cards=recipient_free_cards_offer)
         log.info("Trade completed successfully")
 
     @staticmethod
@@ -400,13 +415,13 @@ class Game:
         log.info(f"{player.name}'s properties:")
         for i, p in enumerate(player.properties):
             log.debug(f"  [{i}] {p.name}")
-        offer_props_selection = input("Enter indices of properties to offer (comma separated): ")
+        offer_spaces_selection = input("Enter indices of properties to offer (comma separated): ")
         try:
-            indices = [int(i.strip()) for i in offer_props_selection.split(",") if i.strip()]
-            offer_props = [player.properties[i] for i in indices if 0 <= i < len(player.properties)]
+            indices = [int(i.strip()) for i in offer_spaces_selection.split(",") if i.strip()]
+            offer_spaces = [player.properties[i] for i in indices if 0 <= i < len(player.properties)]
         except Exception:
             log.warning("Invalid selection")
-            offer_props = []
+            offer_spaces = []
 
         # Offer cash.
         try:
@@ -433,49 +448,84 @@ class Game:
             except ValueError:
                 offer_free_cards = 0
 
-        return offer_props, offer_cash, offer_free_cards
+        return offer_spaces, offer_cash, offer_free_cards
 
-    @staticmethod
-    def execute_trade(p1, p1_properties, p1_cash, p1_free_cards, p2, p2_properties, p2_cash, p2_free_cards):
+    def execute_trade(self, player1: Player, player1_spaces: list, player1_cash: int, player1_free_cards: int,
+                      player2: Player, player2_spaces: list, player2_cash: int, player2_free_cards: int):
         """
         Executes a trade between two players involving properties, cash and 'Get out of jail free' cards. This method
         transfers ownership of the specified properties and adjusts the cash/cards balances of both players according to
         the trade agreement.
 
-        :param p1: The first player involved in the trade.
-        :param p1_properties: List of Property objects to transfer from p1 to p2.
-        :param p1_cash: Amount of cash p1 gives to p2.
-        :param p1_free_cards: Amount of 'Get out of jail free' cards p1 gives to p2.
-        :param p2: The second player involved in the trade.
-        :param p2_properties: List of Property objects to transfer from p2 to p1.
-        :param p2_cash: Amount of cash p2 gives to p1.
-        :param p2_free_cards: Amount of 'Get out of jail free' cards p2 gives to p1.
+        :param player1: The first player involved in the trade.
+        :param player1_spaces: List of Property objects to transfer from player1 to player2.
+        :param player1_cash: Amount of cash player1 gives to player2.
+        :param player1_free_cards: Amount of 'Get out of jail free' cards player1 gives to player2.
+        :param player2: The second player involved in the trade.
+        :param player2_spaces: List of Property objects to transfer from player2 to player1.
+        :param player2_cash: Amount of cash player2 gives to player1.
+        :param player2_free_cards: Amount of 'Get out of jail free' cards player2 gives to player1.
         """
 
-        # Transfer properties.
-        for prop in p1_properties:
-            p1.properties.remove(prop)
-            prop.owner = p2
-            p2.properties.append(prop)
-
-        for prop in p2_properties:
-            p2.properties.remove(prop)
-            prop.owner = p1
-            p1.properties.append(prop)
-
         # Transfer cash.
-        p1.cash -= p1_cash
-        p2.cash += p1_cash
 
-        p2.cash -= p2_cash
-        p1.cash += p2_cash
+        # From player 1 to player 2.
+        player1.cash -= player1_cash
+        player2.cash += player1_cash
+        # From player 2 to player 1.
+        player2.cash -= player2_cash
+        player1.cash += player2_cash
 
         # Transfer 'Get out of jail free' cards.
-        p1.free_cards -= p1_free_cards
-        p2.free_cards += p1_free_cards
 
-        p2.free_cards -= p2_free_cards
-        p1.free_cards += p2_free_cards
+        # From player 1 to player 2.
+        player1.free_cards -= player1_free_cards
+        player2.free_cards += player1_free_cards
+        # From player 2 to player 1.
+        player2.free_cards -= player2_free_cards
+        player1.free_cards += player2_free_cards
+
+        # Transfer spaces.
+
+        # Transfer spaces from player 1 to player 2.
+        self.transfer_spaces(sender=player1, recipient=player2, spaces_to_transfer=player1_spaces)
+        self.transfer_spaces(sender=player2, recipient=player1, spaces_to_transfer=player2_spaces)
+
+    @staticmethod
+    def transfer_spaces(sender: Player, recipient: Player, spaces_to_transfer: list):
+        """
+        TODO: Complete the docstring.
+        """
+
+        # Calculate cash buffer (to redeem mortgaged properties).
+        recipient_mortgage_fee = sum([0.1 * space.mortgage_value for space in spaces_to_transfer if space.is_mortgaged])
+        recipient_cash_buffer = recipient.cash - recipient_mortgage_fee
+
+        # Transfer properties from sender to recipient.
+        for prop in spaces_to_transfer:
+            sender.properties.remove(prop)
+            prop.owner = recipient
+            recipient.properties.append(prop)
+
+            # Recipient to choose between redeeming or paying mortgage fee for mortgaged properties.
+            if prop.is_mortgaged:
+                if prop.redeem_value < recipient_cash_buffer:
+                    action = input("Redeem property or pay mortgage fee (10%): ")
+                    while True:
+                        match action:
+                            case "y":
+                                # Redeem space.
+                                recipient.cash -= prop.redeem_value
+                                prop.is_mortgaged = False
+                                recipient_cash_buffer += (- prop.redeem_value) + 0.1 * prop.mortgage_value
+                            case "n":
+                                # Pay mortgage fee (10%).
+                                recipient.cash -= 0.1 * prop.mortgage_value
+                            case _:
+                                log.warning("Invalid option, choose again")
+                else:
+                    # Recipient doesn't have enough cash to redeem. Pays mortgage fee (10%).
+                    recipient.cash -= 0.1 * prop.mortgage_value
 
     # Development functionality #
 
